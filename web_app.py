4d78fa7f969838023063dfea6bd174601fee9a60
Nick Williams <42781568+washingtonBrewologist@users.noreply.github.com>
2019-09-09 11:58:45 -0700

    Create web_app.py

---
+++ web_app.py
@@ -0,0 +1,29 @@
+import Queue
+import threading
+import os
+import urllib2
+
+threads = 10
+target  = "http://www.blackhatpython.com"
+directory = "/Users/justin/Downloads/joomla-3.1.1"
+filters = [".jpg",".gif",".css"]
+
+os.chdir(directory)
+
+web_paths = Queue.Queue()
+
+for r,d,f in os.walk("."):
+    for files in f:
+        remote_path = "%s%s" % (r, files)
+        if remote_path.startswith("."):
+            remote_path = remote_path[1:]
+        if os.path.splitext(files)[1] not in filters:
+            web_paths.put(remote_path)
+
+def test_remote():
+    while not web_paths.empty():
+          path = web_paths.get()
+          url = "%s%s" % (target, path)
+          
+          request = urllib2.Request(url)
+          
